9d6e6110c9f31d83a4989722f2c0b852e18704a7
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-03-08 15:27:03 -0300

    Documentación

--- pedido.py
+++ pedido.py
@@ -9,9 +9,9 @@ formato = int(input('Ingresa el formato 300g o 500g (sin g): '))
 
 te = Te(sabor, formato)
 
-print('El sabor del té es:',te.obtener_sabor())
+print('El sabor del té es:',te.obtener_sabor()) #no requiere ingreso de parametros ya que usa la información de la instancia
 print('El formato es de',formato,"gramos")
-print('El precio es $',Te.obtener_precio(formato))
+print(f'El precio es ${Te.obtener_precio(formato)}')
 tiempo, recomendacion = Te.obtener_tiempo_recomendacion(sabor)
 print('El tiempo de preparación es',tiempo,'minutos')
 print('Se recomienda',recomendacion)
--- te.py
+++ te.py
@@ -1,31 +1,81 @@
 class Te:
     duracion = 365
     
-    def __init__(self, sabor, formato):
+    def __init__(self, sabor, formato): #Constructor
         self.sabor = sabor
         self.formato = formato
         
     @staticmethod
     def obtener_tiempo_recomendacion(sabor):
-        if sabor == 1: #"té negro":
+        """
+        Define y retorna el tiempo de preparación y recomendadción de consumo del té según su sabor
+        ------------
+        Parameter
+        ------------
+        sabor
+            Type:   int
+            Posibles valores:    1, 2, 3
+        
+        Return
+        ------------
+        tiempo
+            Type:   int
+            Descripción:    Tiempo de preparación del té
+        recomendacion
+            Type:   string
+            Descripción:    Momento del día en que se recomienda su consumo
+        """
+        if sabor == 1: #"té negro"
             tiempo = 3
             recomendacion = "Consumir al desayuno"
-        elif sabor == 2: #"té verde":
+        elif sabor == 2: #"té verde"
             tiempo = 5
             recomendacion = "Consumir al medio día"
-        elif sabor == 3: #"infusión de hierbas":
+        elif sabor == 3: #"infusión de hierbas"
             tiempo = 6
             recomendacion = "Consumir al atardecer"
         return tiempo, recomendacion
     
     @staticmethod
     def obtener_precio(formato):
-        if formato == 300:
-            return 3000
-        elif formato == 500:
-            return 5000
+        """
+        Define y retorna el valor del té según su formato
+        ------------
+        Parameter
+        ------------
+        formato
+            Type:   int
+            Posibles valores:    300 o 500
+            Descripción: Formato de té a comprar que puede ser 300g o 500g
         
-    def obtener_sabor(self):
+        Return
+        ------------
+        precio
+            Type:   int
+            Descripción:    Precio a pagar por el té
+        """
+        if formato == 300:
+            precio = 3000
+        elif formato == 500:
+            precio = 5000
+        return precio
+        
+    def obtener_sabor(self): #self es similar a this en java, se usa para referenciar la instancia actual
+        """
+        Define y retorna el nombre del sabor de té
+        ------------
+        Parameter
+        ------------
+        formato
+            Type:   int
+            Posibles valores:    1, 2, 3
+        
+        Return
+        ------------
+        texto
+            Type:   string
+            Posibles valores:    té negro, té verde o infusión de hierbas
+        """
         if self.sabor == 1:
             texto = "té negro"
         elif self.sabor == 2:
@@ -35,5 +85,6 @@ class Te:
             texto = "infusión de hierbas"
             recomendacion = "Consumir al atardecer"
         return texto
+    
     
     
